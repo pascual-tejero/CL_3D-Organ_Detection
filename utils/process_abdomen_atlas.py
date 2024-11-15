5b79b688f8f0654d97cb526b7d1c0cd02cc41036
Pascual Tejero Cervera <pascu1998@hotmail.com>
2024-11-16 00:01:37 +0100

    feat: add training for ABDOMEN_ATLAS

--- utils/process_abdomen_atlas.py
+++ utils/process_abdomen_atlas.py
@@ -37,6 +37,9 @@ def apply_resizing(data, new_shape, mode_interpolation="trilinear"):
     return data
 
 def process_abdomen_atlas(data_dir):
+
+    error_log_path = Path(data_dir) / "error_log.txt"
+
     # Get directories that start with 'BDMAP' and are directories
     bdmap_dirs = [d for d in os.listdir(data_dir) if d.startswith('BDMAP') and 
                   os.path.isdir(os.path.join(data_dir, d))]
@@ -62,113 +65,127 @@ def process_abdomen_atlas(data_dir):
 
     # Process each directory to create segmentation labels and save as .nii.gz and .npy files
     for dirpath, dirnames, filenames in os.walk(script_dir):
-        if "segmentations" in dirpath:
-            ct_path = Path(dirpath).parent / "ct.nii.gz"
-            ct_nifti = nib.load(str(ct_path))
-            ct_data = ct_nifti.get_fdata()
-            ct_affine = ct_nifti.affine
-            ct_header = ct_nifti.header
-            label_matrix = np.zeros_like(ct_data, dtype=np.uint8)
+        try:
+            if "segmentations" in dirpath:
+                ct_path = Path(dirpath).parent / "ct.nii.gz"
+                ct_nifti = nib.load(str(ct_path))
+                ct_data = ct_nifti.get_fdata()
+                ct_affine = ct_nifti.affine
+                ct_header = ct_nifti.header
+                label_matrix = np.zeros_like(ct_data, dtype=np.uint8)
 
-            # Create label matrix from organ segmentations
-            for filename in filenames:
-                if filename.endswith(".nii.gz"):
-                    organ_name = filename.split(".")[0]
-                    organ_label = ORGAN_TO_LABEL[organ_name]
-                    organ_matrix = nib.load(str(Path(dirpath) / filename)).get_fdata()
-                    label_matrix[organ_matrix > 0] = organ_label
-            label_path = Path(dirpath).parent / "label.nii.gz"
-            nib.save(nib.Nifti1Image(label_matrix, ct_affine, ct_header), label_path)
+                # Create label matrix from organ segmentations
+                for filename in filenames:
+                    if filename.endswith(".nii.gz"):
+                        organ_name = filename.split(".")[0]
+                        organ_label = ORGAN_TO_LABEL[organ_name]
+                        organ_matrix = nib.load(str(Path(dirpath) / filename)).get_fdata()
+                        label_matrix[organ_matrix > 0] = organ_label
+                label_path = Path(dirpath).parent / "label.nii.gz"
+                nib.save(nib.Nifti1Image(label_matrix, ct_affine, ct_header), label_path)
+                print(f"Label matrix label.nii.gz saved in {dirpath}")
 
-            # Save ct and label as .npy (data.npy and label.npy)
-            # np.save(str(Path(dirpath).parent / "data.npy"), ct_data)
-            # np.save(str(Path(dirpath).parent / "label.npy"), label_matrix)
+                # Save ct and label as .npy (data.npy and label.npy)
+                # np.save(str(Path(dirpath).parent / "data.npy"), ct_data)
+                # np.save(str(Path(dirpath).parent / "label.npy"), label_matrix)
+        except Exception as e:
+            with open(error_log_path, "a") as f:
+                print(f"Error processing {dirpath}: {e}")
+                f.write(f"Error processing {dirpath}: {e}\n")
 
     # Resize and crop the data for train, validation, and test sets
     for dirpath, dirnames, filenames in os.walk(data_dir):
         filenames_data_label = [f for f in filenames if f == "ct.nii.gz" or f == "label.nii.gz"]
 
-        if "train" in dirpath and filenames_data_label:
-            ct_path = Path(dirpath) / "ct.nii.gz"
-            ct_nifti = nib.load(str(ct_path))
-            ct_data = ct_nifti.get_fdata()
-            ct_affine = ct_nifti.affine
-            ct_header = ct_nifti.header
+        try:
 
-            label_path = Path(dirpath) / "label.nii.gz"
-            label_nifti = nib.load(str(label_path))
-            label_data = label_nifti.get_fdata()
-            label_affine = label_nifti.affine
-            label_header = label_nifti.header
+            if "train" in dirpath and filenames_data_label:
+                ct_path = Path(dirpath) / "ct.nii.gz"
+                ct_nifti = nib.load(str(ct_path))
+                ct_data = ct_nifti.get_fdata()
+                ct_affine = ct_nifti.affine
+                ct_header = ct_nifti.header
 
-            # Determine the new shape for resizing
-            if ct_data.shape[2] < 32:
-                z_shape = 32 
-            elif ct_data.shape[2] >= 32 and ct_data.shape[2] < 256:
-                z_shape = (ct_data.shape[2] // 32) * 32
-            else:
-                z_shape = 256
-            xyz_shape = (256, 256, z_shape)
-            ct_data = apply_resizing(ct_data, xyz_shape, mode_interpolation="trilinear").astype(np.float32)
-            label_data = apply_resizing(label_data, xyz_shape, mode_interpolation="nearest").astype(np.uint8)
+                label_path = Path(dirpath) / "label.nii.gz"
+                label_nifti = nib.load(str(label_path))
+                label_data = label_nifti.get_fdata()
+                label_affine = label_nifti.affine
+                label_header = label_nifti.header
 
-            # Crop the data and label to the same shape
-            possible_cropping = [160, 192, 224, 256] # Possible cropping sizes of multiple of 32
-            crop_shape = random.choice(possible_cropping)
-            crop_start_x = random.randint(0, xyz_shape[0] - crop_shape)
-            crop_start_y = random.randint(0, xyz_shape[1] - crop_shape)
+                # Determine the new shape for resizing
+                if ct_data.shape[2] < 32:
+                    z_shape = 32 
+                elif ct_data.shape[2] >= 32 and ct_data.shape[2] < 256:
+                    z_shape = (ct_data.shape[2] // 32) * 32
+                else:
+                    z_shape = 256
+                xyz_shape = (256, 256, z_shape)
+                ct_data = apply_resizing(ct_data, xyz_shape, mode_interpolation="trilinear").astype(np.float32)
+                label_data = apply_resizing(label_data, xyz_shape, mode_interpolation="nearest").astype(np.uint8)
 
-            ct_data = ct_data[crop_start_x:crop_start_x + crop_shape, crop_start_y:crop_start_y + crop_shape, :]
-            label_data = label_data[crop_start_x:crop_start_x + crop_shape, crop_start_y:crop_start_y + crop_shape, :]
-            
-            # Save resized and cropped nifti files
-            ct_path = Path(dirpath) / "ct_resized_cropped.nii.gz"
-            nib.save(nib.Nifti1Image(ct_data, ct_affine, ct_header), ct_path)
-            label_path = Path(dirpath) / "label_resized_cropped.nii.gz"
-            nib.save(nib.Nifti1Image(label_data, label_affine, label_header), label_path)
+                # Crop the data and label to the same shape
+                possible_cropping = [160, 192, 224, 256] # Possible cropping sizes of multiple of 32
+                crop_shape = random.choice(possible_cropping)
+                crop_start_x = random.randint(0, xyz_shape[0] - crop_shape)
+                crop_start_y = random.randint(0, xyz_shape[1] - crop_shape)
 
-            # Save resized and cropped data and label as .npy (data_resized_cropped.npy and label_resized_cropped.npy)
-            # np.save(str(Path(dirpath) / "ct_resized_cropped.npy"), ct_data)
-            # np.save(str(Path(dirpath) / "label_resized_cropped.npy"), label_data)
-            np.save(str(Path(dirpath) / "ct.npy"), ct_data)
-            np.save(str(Path(dirpath) / "label.npy"), label_data)
+                ct_data = ct_data[crop_start_x:crop_start_x + crop_shape, crop_start_y:crop_start_y + crop_shape, :]
+                label_data = label_data[crop_start_x:crop_start_x + crop_shape, crop_start_y:crop_start_y + crop_shape, :]
+                
+                # Save resized and cropped nifti files
+                ct_path = Path(dirpath) / "ct_resized_cropped.nii.gz"
+                nib.save(nib.Nifti1Image(ct_data, ct_affine, ct_header), ct_path)
+                label_path = Path(dirpath) / "label_resized_cropped.nii.gz"
+                nib.save(nib.Nifti1Image(label_data, label_affine, label_header), label_path)
 
-        elif ("val" in dirpath or "test" in dirpath) and filenames_data_label:            
-            ct_path = Path(dirpath) / "ct.nii.gz"
-            ct_nifti = nib.load(str(ct_path))
-            ct_data = ct_nifti.get_fdata()
-            ct_affine = ct_nifti.affine
-            ct_header = ct_nifti.header
+                # Save resized and cropped data and label as .npy (data_resized_cropped.npy and label_resized_cropped.npy)
+                # np.save(str(Path(dirpath) / "ct_resized_cropped.npy"), ct_data)
+                # np.save(str(Path(dirpath) / "label_resized_cropped.npy"), label_data)
+                np.save(str(Path(dirpath) / "ct.npy"), ct_data)
+                np.save(str(Path(dirpath) / "label.npy"), label_data)
+                print(f"Files ct.npy and label.npy saved in {dirpath}")
 
-            label_path = Path(dirpath) / "label.nii.gz"
-            label_nifti = nib.load(str(label_path))
-            label_data = label_nifti.get_fdata()
-            label_affine = label_nifti.affine
-            label_header = label_nifti.header
+            elif ("val" in dirpath or "test" in dirpath) and filenames_data_label:            
+                ct_path = Path(dirpath) / "ct.nii.gz"
+                ct_nifti = nib.load(str(ct_path))
+                ct_data = ct_nifti.get_fdata()
+                ct_affine = ct_nifti.affine
+                ct_header = ct_nifti.header
 
-            # Determine the new shape for resizing
-            if ct_data.shape[2] < 32:
-                z_shape = 32 
-            elif ct_data.shape[2] >= 32 and ct_data.shape[2] < 256:
-                z_shape = (ct_data.shape[2] // 32) * 32
-            else:
-                z_shape = 256
-            xyz_shape = (256, 256, z_shape)
-            ct_data = apply_resizing(ct_data, xyz_shape, mode_interpolation="trilinear").astype(np.float32)
-            label_data = apply_resizing(label_data, xyz_shape, mode_interpolation="nearest").astype(np.uint8)
-            
-            # Save resized and cropped nifti files
-            ct_path = Path(dirpath) / "ct_resized_cropped.nii.gz"
-            nib.save(nib.Nifti1Image(ct_data, ct_affine, ct_header), ct_path)
-            label_path = Path(dirpath) / "label_resized_cropped.nii.gz"
-            nib.save(nib.Nifti1Image(label_data, label_affine, label_header), label_path)
+                label_path = Path(dirpath) / "label.nii.gz"
+                label_nifti = nib.load(str(label_path))
+                label_data = label_nifti.get_fdata()
+                label_affine = label_nifti.affine
+                label_header = label_nifti.header
 
-            # Save resized and cropped data and label as .npy (data_resized_cropped.npy and label_resized_cropped.npy)
-            # np.save(str(Path(dirpath) / "ct_resized_cropped.npy"), ct_data)
-            # np.save(str(Path(dirpath) / "label_resized_cropped.npy"), label_data)
-            np.save(str(Path(dirpath) / "ct.npy"), ct_data)
-            np.save(str(Path(dirpath) / "label.npy"), label_data)
-        
+                # Determine the new shape for resizing
+                if ct_data.shape[2] < 32:
+                    z_shape = 32 
+                elif ct_data.shape[2] >= 32 and ct_data.shape[2] < 256:
+                    z_shape = (ct_data.shape[2] // 32) * 32
+                else:
+                    z_shape = 256
+                xyz_shape = (256, 256, z_shape)
+                ct_data = apply_resizing(ct_data, xyz_shape, mode_interpolation="trilinear").astype(np.float32)
+                label_data = apply_resizing(label_data, xyz_shape, mode_interpolation="nearest").astype(np.uint8)
+                
+                # Save resized and cropped nifti files
+                ct_path = Path(dirpath) / "ct_resized_cropped.nii.gz"
+                nib.save(nib.Nifti1Image(ct_data, ct_affine, ct_header), ct_path)
+                label_path = Path(dirpath) / "label_resized_cropped.nii.gz"
+                nib.save(nib.Nifti1Image(label_data, label_affine, label_header), label_path)
+
+                # Save resized and cropped data and label as .npy (data_resized_cropped.npy and label_resized_cropped.npy)
+                # np.save(str(Path(dirpath) / "ct_resized_cropped.npy"), ct_data)
+                # np.save(str(Path(dirpath) / "label_resized_cropped.npy"), label_data)
+                np.save(str(Path(dirpath) / "ct.npy"), ct_data)
+                np.save(str(Path(dirpath) / "label.npy"), label_data)
+                print(f"Files ct.npy and label.npy saved in {dirpath}")
+
+        except Exception as e:
+            with open(error_log_path, "a") as f:
+                print(f"Error processing {dirpath}: {e}")
+                f.write(f"Error processing {dirpath}: {e}\n")
         filenames_data_label = []
 
 
